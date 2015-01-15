7a548c2cb15f4c915c24427bf6a34858ee0ffed9
Joe Sak <joe@joesak.com>
2015-01-15 11:40:26 -0500

    Add HarvestClient initializer

---
+++ config/initializers/harvest_client.rb
@@ -0,0 +1,3 @@
+HarvestClient = Harvest.hardy_client(subdomain: ENV['HARVEST_SUBDOMAIN'],
+                                     username: ENV['HARVEST_USERNAME'],
+                                     password: ENV['HARVEST_PASSWORD'])
