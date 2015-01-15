ae08990b48b0d9c8e4acfc34f4f37260496d0ce0
Joe Sak <joe@joesak.com>
2015-01-15 12:19:08 -0500

    Add PtHarvest::TrackTimeOnStories

---
+++ app/domain/pt_harvest/track_time_on_stories.rb
@@ -0,0 +1,31 @@
+module PtHarvest
+  module TrackTimeOnStories
+    def self.story(time)
+      story_id = story_id(time)
+      project_id = project_id(time)
+      PivotalTracker::Story.find(story_id, project_id)
+    end
+
+    def self.time_referencing_stories
+      time = HarvestClient.time.all
+      time.keep_if { |t| t.notes.match /#\d+/ }
+      time
+    end
+
+    private
+    def self.project_id(time)
+      time_project = time_project(time)
+      id = time_project.notes.match(/\[PT#(\d+)\]/)[1]
+      id.to_i
+    end
+
+    def self.story_id(time)
+      id = time.notes.match(/#(\d+)/)[1]
+      id.to_i
+    end
+
+    def self.time_project(time)
+      HarvestClient.projects.find(time.project_id)
+    end
+  end
+end
